b21302d78c36298d094c696c1cc62a491a4be85c
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-11-13 23:01:50 +0100

    added exec command to setup_dev_env

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -7,35 +7,39 @@ project_name="ticket_app_dev"
 # POSTGRES_PORT=5432
 # REDIS_PORT=6379
 
+ask_to_close() {
+    echo "dev environment already running"
+    while true; do
+        read -p "Do you want to take it down? (y/n) " yn
+        case $yn in 
+            [yY] )
+                docker compose -p $project_name down
+                exit 0
+                ;;
+
+            [nN] )
+                echo exiting...
+                exit 0
+                ;;
+
+            * )
+                echo invalid response
+                ;;
+        esac
+    done
+}
+
 already_running=$(docker compose ls --filter name="$project_name" | wc -l)
 ((already_running--))
 
 if [ $already_running -eq 1 ]; then
-    echo "dev environment already running"
-
-    while true; do
-
-    read -p "Do you want to take it down? (y/n) " yn
-
-    case $yn in 
-        [yY] )
-            docker compose -p $project_name down
-            exit 0
-            ;;
-
-        [nN] )
-            echo exiting...
-            exit 0
-            ;;
-
-        * )
-            echo invalid response
-            ;;
-    esac
-
-    done
+    if [[ $1 = "exec" && ( $2 = "redis" || $2 = "db" ) ]];then
+    docker exec -it "${project_name}-${2}-1" bash
+    else
+        ask_to_close
+    fi
 
     exit 1
 fi
 
-docker compose -p $project_name up
+docker compose -p $project_name up -d
