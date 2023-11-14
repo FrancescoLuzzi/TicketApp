ee4fd6846dbad3bcad2e49154d6b9d70d2a345f8
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-11-14 22:57:20 +0100

    fixup setup_dev_env

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -1,25 +1,31 @@
 project_name="ticket_app_dev"
 
 ## env environment modifiers
-# POSTGRES_USER="user"
-# POSTGRES_PASSWORD="password"
-# POSTGRES_DB="db"
-# POSTGRES_PORT=5432
-# REDIS_PORT=6379
+POSTGRES_USER="user"
+POSTGRES_PASSWORD="password"
+POSTGRES_DB="ticket_app"
+POSTGRES_PORT=5432
+REDIS_PORT=6379
+
+DB_HOST="localhost"
+
+export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DB_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 
 ask_to_close() {
     echo "dev environment already running"
     while true; do
-        read -p "Do you want to take it down? (y/n) " yn
+        read -p "Do you want to take it down? (Y/n) " yn
         case $yn in 
-            [yY] )
+            y|Y|"" )
                 docker compose -p $project_name down
-                exit 0
+                return 0
                 ;;
 
-            [nN] )
+            n|N )
                 echo exiting...
-                exit 0
+                sqlx database create
+                sqlx migrate run
+                return 0
                 ;;
 
             * )
@@ -29,17 +35,42 @@ ask_to_close() {
     done
 }
 
+ask_force_recreate() {
+    read -p "Force recreation of the containers? (y/N) " yn
+    while true; do
+        case $yn in 
+            y|Y )
+                docker compose -p $project_name up -d --force-recreate
+                break 2
+                ;;
+
+            n|N|"" )
+                docker compose -p $project_name up -d
+                break 2
+                ;;
+
+            * )
+                echo invalid response
+                ;;
+        esac
+    done
+    sqlx database create
+    sqlx migrate run
+}
+
 already_running=$(docker compose ls --filter name="$project_name" | wc -l)
 ((already_running--))
 
 if [ $already_running -eq 1 ]; then
     if [[ $1 = "exec" && ( $2 = "redis" || $2 = "db" ) ]];then
-    docker exec -it "${project_name}-${2}-1" bash
+        docker exec -it "${project_name}-${2}-1" bash
+        exit 0
     else
         ask_to_close
+        exit 0 
     fi
 
     exit 1
 fi
 
-docker compose -p $project_name up -d
+ask_force_recreate
