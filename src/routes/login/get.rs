cba3464b8acb913af600ffa077e3e121e124ae38
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-11-19 20:32:59 +0100

    small templating to display existing users

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -2,20 +2,39 @@ use crate::app_state::SharedAppState;
 use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
-use axum::Json;
+use secrecy::SecretString;
 use std::ops::DerefMut;
 use uuid::Uuid;
 
-#[derive(sqlx::FromRow, serde::Serialize)]
+#[derive(sqlx::FromRow, askama::Template)]
+#[template(path = "user_read.html")]
 struct User {
     id: Uuid,
     username: String,
-    password: String,
+    password: SecretString,
     email: String,
 }
 
+#[derive(askama::Template)]
+#[template(
+    source = r#"
+<div class="users_container>">
+    {% for item in users %}
+    {{ item }}
+    {% endfor %}
+</div>
+"#,
+    ext = "html",
+    escape = "none"
+)]
+struct Users<'a> {
+    users: &'a Vec<User>,
+}
+
+#[tracing::instrument(skip_all)]
 pub async fn get(State(state): State<SharedAppState>) -> Result<impl IntoResponse, StatusCode> {
-    let mut transaction = state.clone()
+    let mut transaction = state
+        .clone()
         .db_pool
         .begin()
         .await
@@ -24,6 +43,10 @@ pub async fn get(State(state): State<SharedAppState>) -> Result<impl IntoRespons
         .fetch_all(transaction.deref_mut())
         .await
         .map_err(|_| StatusCode::NOT_FOUND)?;
-    transaction.commit().await.map_err(|_|StatusCode::INTERNAL_SERVER_ERROR)?;
-    Ok((StatusCode::OK, Json(users)))
+    transaction
+        .commit()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let users_out = Users { users: &users };
+    Ok((StatusCode::OK, users_out.into_response()))
 }
